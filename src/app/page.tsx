eb780a31207f0c1400c7c1d809827f4e3f627cb7
mhmtaltnts <12mart1973@gmail.com>
2023-11-04 22:28:12 +0300

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Header from '@/components/header';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <h1>Home Page</h1>
+      <p>Selam Kelam </p>
+      <h3>Selam</h3>
+      <div className="flex gap-1 text-blue-600">
+        <Link href="/login" className="underline">
+          Login
+        </Link>
+        <Link href="/register" className="underline">
+          Register
+        </Link>
+        <Link href="/profile" className="underline">
+          Profile
+        </Link>
+      </div>
+    </main>
+  );
+}
